d104f97436f73f7ed61e3d74adf37e00f4935e6f
EmanoelyCSI <88907064+EmanoelyCSI@users.noreply.github.com>
2021-09-09 21:33:11 -0300

    up

--- database/seeders/PostoSeeder.php
+++ database/seeders/PostoSeeder.php
@@ -16,31 +16,31 @@ class PostoSeeder extends Seeder
     {
         //
         Posto::create([
-            'cnpj'=>'07.739.067/0001-91',
-            'name'=> 'Shell Estácio',
-            'email'=> ' shell.estacio-contato@gmail.com',
-            'cell'=>'2198645-8261',
-            'tel'=>'212546-8965',
-            'address'=>'R. Estácio de Sá, 87 - Estácio, Rio de Janeiro - RJ, 20211-270',
-            'bomba'=>'4',
-            'bico'=>'2',
-            'turno'=>'4',
-            'cicloTurno'=>'6 horas',
+            'cnpj'        => '07.739.067/0001-91',
+            'name'        => 'Shell Estácio',
+            'email'       => ' shell.estacio-contato@gmail.com',
+            'cell'        => '2198645-8261',
+            'tel'         => '212546-8965',
+            'address'     => 'R. Estácio de Sá, 87 - Estácio, Rio de Janeiro - RJ, 20211-270',
+            'bomba'       => '4',
+            'bico'        => '2',
+            'turno'       => '4',
+            'cicloTurno'  => '6 horas',
             'contrato_id' => '1',
         ]);
 
         Posto::create([
-            'cnpj'=>'05.418.078/0001-92',
-            'name'=> 'Ipiranga Humaitá',
-            'email'=> 'ipiranga_humaita_contato@gmail.com',
-            'cell'=>'2198745-8272',
-            'tel'=>'212545-8875',
-            'address'=>'R. Humaitá,136 - Humaitá Rio de Janeiro - RJ 22261-004',
-            'bomba'=>'2',
-            'bico'=>'2',
-            'turno'=>'3',
-            'cicloTurno'=>'8 horas',
-            'contrato_id' => '1',
+            'cnpj'          => '05.418.078/0001-92',
+            'name'          => 'Ipiranga Humaitá',
+            'email'         => 'ipiranga_humaita_contato@gmail.com',
+            'cell'          => '2198745-8272',
+            'tel'           => '212545-8875',
+            'address'       => 'R. Humaitá,136 - Humaitá Rio de Janeiro - RJ 22261-004',
+            'bomba'         => '2',
+            'bico'          => '2',
+            'turno'         => '3',
+            'cicloTurno'    => '8 horas',
+            'contrato_id'   => '1',
         ]);
     }
 }
